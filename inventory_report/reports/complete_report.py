8377213438ab96a1d34aadcde1792439ea6e4cc0
Anderson-Reges <andersonreges76@gmail.com>
2023-05-08 19:18:38 -0300

    Feat: implementando um metodo estatico em complete_report e refatorando simple_report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,22 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(product_list: list[dict]):
+        simple = super(CompleteReport, CompleteReport).generate(product_list)
+        complete = dict()
+        complete_return = ''
+        for product in product_list:
+            if product["nome_da_empresa"] in complete:
+                complete[product["nome_da_empresa"]] += 1
+            else:
+                complete[product["nome_da_empresa"]] = 1
+
+        for company in complete.items():
+            complete_return += f"- {company[0]}: {company[1]}\n"
+        return (
+            f'{simple}'
+            f'\nProdutos estocados por empresa:\n'
+            f'{complete_return}'
+        )
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -5,26 +5,25 @@ class SimpleReport:
     @staticmethod
     def generate(product_list: list[dict]):
         actual_date = datetime.now().strftime('%Y-%m-%d')
-        old_fabrication = min(
+        old_fabrication: int = min(
             [
                 product['data_de_fabricacao'] for product in product_list
             ]
         )
-        old_validity = min(
+        old_validity: int = min(
             [
                 product['data_de_validade'] for product in product_list
                 if product['data_de_validade'] > actual_date
             ],
             default="EMPTY"
         )
-        company_name = max(
+        company_name: str = max(
             company['nome_da_empresa'] for company in product_list
         )
+        company_name_rm_limited = company_name.replace('LIMITED', '').strip()
 
         return (
-            f"""
-            Data de fabricação mais antiga: {old_fabrication}
-            Data de validade mais próxima: {old_validity}
-            Empresa com mais produtos: {company_name}
-            """
+            f"Data de fabricação mais antiga: {old_fabrication}\n"
+            f"Data de validade mais próxima: {old_validity}\n"
+            f"Empresa com mais produtos: {company_name_rm_limited}"
         )
