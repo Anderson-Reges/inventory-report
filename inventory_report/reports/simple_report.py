b3632e47af8581b488de8c4b9c9dc301b458c058
Anderson-Reges <andersonreges76@gmail.com>
2023-05-08 16:51:02 -0300

    Feat: implementando um metodo estatico na classe simple_report

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,30 @@
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(product_list: list[dict]):
+        actual_date = datetime.now().strftime('%Y-%m-%d')
+        old_fabrication = min(
+            [
+                product['data_de_fabricacao'] for product in product_list
+            ]
+        )
+        old_validity = min(
+            [
+                product['data_de_validade'] for product in product_list
+                if product['data_de_validade'] > actual_date
+            ],
+            default="EMPTY"
+        )
+        company_name = max(
+            company['nome_da_empresa'] for company in product_list
+        )
+
+        return (
+            f"""
+            Data de fabricação mais antiga: {old_fabrication}
+            Data de validade mais próxima: {old_validity}
+            Empresa com mais produtos: {company_name}
+            """
+        )
