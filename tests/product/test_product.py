60ff4f10599328156d704acde448c8eb3974cfca
Anderson-Reges <andersonreges76@gmail.com>
2023-05-08 14:57:25 -0300

    Feat: testando o construtor da classe Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,15 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1, "Bolacha", "Trakinus", "20/12/2022",
+        "20/12/2023", "2ZSD5F1FD5", "Em local seco"
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == "Bolacha"
+    assert product.nome_da_empresa == "Trakinus"
+    assert product.data_de_fabricacao == "20/12/2022"
+    assert product.data_de_validade == "20/12/2023"
+    assert product.numero_de_serie == "2ZSD5F1FD5"
+    assert product.instrucoes_de_armazenamento == "Em local seco"
