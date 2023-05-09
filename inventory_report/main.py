6802f3b448c34c42cd7094b797722da46429130e
Anderson-Reges <andersonreges76@gmail.com>
2023-05-09 04:43:37 -0300

    Feat: implementando o metodo main

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,16 @@
+import sys
+from inventory_report.inventory.inventory import Inventory
+
+
 def main():
-    pass
+    try:
+        input_path = sys.argv[1]
+        report_type = sys.argv[2]
+        report = Inventory.import_data(input_path, report_type)
+        sys.stdout.write(report)
+    except IndexError:
+        sys.stderr.write("Verifique os argumentos\n")
+
+
+if __name__ == "__main__":
+    main()
