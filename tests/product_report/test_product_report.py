2f9adcf86dbb467041bacb3f053afdfd5046f304
Anderson-Reges <andersonreges76@gmail.com>
2023-05-09 02:22:40 -0300

    Feat: implementando o teste de relatorio individual

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,10 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1, "farinha", "Farinini", "01-05-2021",
+        "02-06-2023", "F2Y2", "ao abrigo de luz"
+    )
+    result = product.__repr__()
+    assert result == "O produto farinha fabricado em 01-05-2021 por Farinini com validade até 02-06-2023 precisa ser armazenado ao abrigo de luz."
